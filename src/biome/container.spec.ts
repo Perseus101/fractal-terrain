6c51264cfa5e9146cd21fb9abbccb224c90ea625
Colin Moore <colin@moore.one>
2019-05-01 01:18:49 -0400

    Fix unit tests

--- src/biome/container.spec.ts
+++ src/biome/container.spec.ts
@@ -9,16 +9,16 @@ import Biome from './biome';
 describe('BiomeContainer', () => {
     it('Should hold several biomes', () => {
         let biomes = new BiomeContainer([
-            new Biome(vec3.fromValues(1, 0, 0), 1),
-            new Biome(vec3.fromValues(0, 1, 0), 0),
-            new Biome(vec3.fromValues(0, 0, 1), 0.5),
+            new Biome(vec3.fromValues(1, 0, 0), 1, 0.99),
+            new Biome(vec3.fromValues(0, 1, 0), 0, 0.99),
+            new Biome(vec3.fromValues(0, 0, 1), 0.5, 0.99),
         ]);
         expect(biomes).to.not.be.null;
     });
     it('Should interpolate between biomes', () => {
-        let biome1 = new Biome(vec3.fromValues(1, 0, 0), 1)
-        let biome2 = new Biome(vec3.fromValues(0, 1, 0), 0)
-        let biome3 = new Biome(vec3.fromValues(0, 0, 1), 0.5)
+        let biome1 = new Biome(vec3.fromValues(1, 0, 0), 1, 0.99)
+        let biome2 = new Biome(vec3.fromValues(0, 1, 0), 0, 0.99)
+        let biome3 = new Biome(vec3.fromValues(0, 0, 1), 0.5, 0.99)
 
         let biomes = new BiomeContainer([biome1, biome2, biome3]);
         expect(biomes.createInterpolatedBiome([1, 0, 0])).to.eql(biome1);
@@ -33,10 +33,10 @@ describe('BiomeContainer', () => {
 
 describe('BiomeQuad', () => {
     it('interpolates between biomes', () => {
-        let biome1 = new Biome(vec3.fromValues(0, 0, 0), 0);
-        let biome2 = new Biome(vec3.fromValues(0, 1, 0), 0.25);
-        let biome3 = new Biome(vec3.fromValues(1, 0, 0), 0.5);
-        let biome4 = new Biome(vec3.fromValues(1, 1, 0), 0.75);
+        let biome1 = new Biome(vec3.fromValues(0, 0, 0), 0, 0.99);
+        let biome2 = new Biome(vec3.fromValues(0, 1, 0), 0.25, 0.99);
+        let biome3 = new Biome(vec3.fromValues(1, 0, 0), 0.5, 0.99);
+        let biome4 = new Biome(vec3.fromValues(1, 1, 0), 0.75, 0.99);
 
         let biomes = new BiomeContainer([biome1, biome2, biome3, biome4]);
 
@@ -57,16 +57,16 @@ describe('BiomeQuad', () => {
         expect(quad.bilerp(vec3.fromValues(1, 0, 0))).to.eql(biome3);
         expect(quad.bilerp(vec3.fromValues(1, 0, 1))).to.eql(biome4);
 
-        expect(quad.bilerp(vec3.fromValues(0, 0, 0.5))).to.eql(new Biome(vec3.fromValues(0, 0.5, 0), 0.125));
-        expect(quad.bilerp(vec3.fromValues(0.5, 0, 0))).to.eql(new Biome(vec3.fromValues(0.5, 0, 0), 0.25));
-        expect(quad.bilerp(vec3.fromValues(0.5, 0, 0.5))).to.eql(new Biome(vec3.fromValues(0.5, 0.5, 0), 0.375));
+        expect(quad.bilerp(vec3.fromValues(0, 0, 0.5))).to.eql(new Biome(vec3.fromValues(0, 0.5, 0), 0.125, 0.99));
+        expect(quad.bilerp(vec3.fromValues(0.5, 0, 0))).to.eql(new Biome(vec3.fromValues(0.5, 0, 0), 0.25, 0.99));
+        expect(quad.bilerp(vec3.fromValues(0.5, 0, 0.5))).to.eql(new Biome(vec3.fromValues(0.5, 0.5, 0), 0.375, 0.99));
     });
 
     it('interpolates correctly in negative quadrant', () => {
-        let biome1 = new Biome(vec3.fromValues(0, 0, 0), 0);
-        let biome2 = new Biome(vec3.fromValues(0, 1, 0), 0.25);
-        let biome3 = new Biome(vec3.fromValues(1, 0, 0), 0.5);
-        let biome4 = new Biome(vec3.fromValues(1, 1, 0), 0.75);
+        let biome1 = new Biome(vec3.fromValues(0, 0, 0), 0, 0.99);
+        let biome2 = new Biome(vec3.fromValues(0, 1, 0), 0.25, 0.99);
+        let biome3 = new Biome(vec3.fromValues(1, 0, 0), 0.5, 0.99);
+        let biome4 = new Biome(vec3.fromValues(1, 1, 0), 0.75, 0.99);
 
         let biomes = new BiomeContainer([biome1, biome2, biome3, biome4]);
 
@@ -82,17 +82,17 @@ describe('BiomeQuad', () => {
             vec3.fromValues(-1, 0, -1)
         ]);
 
-        expect(quad.bilerp(vec3.fromValues(0, 0, -0.5))).to.eql(new Biome(vec3.fromValues(0, 0.5, 0), 0.125));
-        expect(quad.bilerp(vec3.fromValues(-0.5, 0, 0))).to.eql(new Biome(vec3.fromValues(0.5, 0, 0), 0.25));
-        expect(quad.bilerp(vec3.fromValues(-0.5, 0, -0.5))).to.eql(new Biome(vec3.fromValues(0.5, 0.5, 0), 0.375));
+        expect(quad.bilerp(vec3.fromValues(0, 0, -0.5))).to.eql(new Biome(vec3.fromValues(0, 0.5, 0), 0.125, 0.99));
+        expect(quad.bilerp(vec3.fromValues(-0.5, 0, 0))).to.eql(new Biome(vec3.fromValues(0.5, 0, 0), 0.25, 0.99));
+        expect(quad.bilerp(vec3.fromValues(-0.5, 0, -0.5))).to.eql(new Biome(vec3.fromValues(0.5, 0.5, 0), 0.375, 0.99));
     });
 
 
     it('interpolates correctly in pos/neg quadrant', () => {
-        let biome1 = new Biome(vec3.fromValues(0, 0, 0), 0);
-        let biome2 = new Biome(vec3.fromValues(0, 1, 0), 0.25);
-        let biome3 = new Biome(vec3.fromValues(1, 0, 0), 0.5);
-        let biome4 = new Biome(vec3.fromValues(1, 1, 0), 0.75);
+        let biome1 = new Biome(vec3.fromValues(0, 0, 0), 0, 0.99);
+        let biome2 = new Biome(vec3.fromValues(0, 1, 0), 0.25, 0.99);
+        let biome3 = new Biome(vec3.fromValues(1, 0, 0), 0.5, 0.99);
+        let biome4 = new Biome(vec3.fromValues(1, 1, 0), 0.75, 0.99);
 
         let biomes = new BiomeContainer([biome1, biome2, biome3, biome4]);
 
@@ -108,8 +108,8 @@ describe('BiomeQuad', () => {
             vec3.fromValues(1, 0, -1)
         ]);
 
-        expect(quad.bilerp(vec3.fromValues(0, 0, -0.5))).to.eql(new Biome(vec3.fromValues(0, 0.5, 0), 0.125));
-        expect(quad.bilerp(vec3.fromValues(0.5, 0, 0))).to.eql(new Biome(vec3.fromValues(0.5, 0, 0), 0.25));
-        expect(quad.bilerp(vec3.fromValues(0.5, 0, -0.5))).to.eql(new Biome(vec3.fromValues(0.5, 0.5, 0), 0.375));
+        expect(quad.bilerp(vec3.fromValues(0, 0, -0.5))).to.eql(new Biome(vec3.fromValues(0, 0.5, 0), 0.125, 0.99));
+        expect(quad.bilerp(vec3.fromValues(0.5, 0, 0))).to.eql(new Biome(vec3.fromValues(0.5, 0, 0), 0.25, 0.99));
+        expect(quad.bilerp(vec3.fromValues(0.5, 0, -0.5))).to.eql(new Biome(vec3.fromValues(0.5, 0.5, 0), 0.375, 0.99));
     });
 });
